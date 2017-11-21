e867c5721a73484c3eb281c67ca1bb79e24644b3
Gemma Garcias <gemma.garcias.e@gmail.com>
2017-11-21 14:15:54 +0100

    finished addcollection v.2

--- src/services/apiSessions.js
+++ src/services/apiSessions.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 export const getGallery = () => {
-	const url = 'http://localhost:3001/gallery'
+	const url = 'https://webfotograph-project.herokuapp.com/gallery'
 	return axios.get(url)
 		.then( response => response.data )
 }
 
 export const getSessionById = (id) => {
-	const url = `http://localhost:3001/gallery/${id}`
+	const url = `https://webfotograph-project.herokuapp.com/gallery/${id}`
 	return axios.get(url)
 		.then( response => response.data )
 }
