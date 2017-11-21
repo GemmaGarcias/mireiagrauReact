803389e69140016c060804160f231198c3f6c255
Gemma Garcias <gemma.garcias.e@gmail.com>
2017-11-21 15:10:13 +0100

    adding collection v.3

--- src/components/adminHandlers/AddCollection.js
+++ src/components/adminHandlers/AddCollection.js
@@ -29,7 +29,7 @@ class AddCollection extends Component {
   handleSubmit(event) {
     alert('your message has been sent')
     event.preventDefault()
-    axios.post('http://localhost:3001/new', {
+    axios.post('https://webfotograph-project.herokuapp.com/new', {
      name: this.state.name,
      detail:this.state.detail,
      img: this.state.img,
