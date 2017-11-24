f02e21d28eeccb0eb63338dc699cbb8ae250280a
Gemma Garcias <gemma.garcias.e@gmail.com>
2017-11-24 12:01:47 +0100

    v.2

--- src/components/adminHandlers/AddCollection.js
+++ src/components/adminHandlers/AddCollection.js
@@ -32,7 +32,7 @@ class AddCollection extends Component {
 
   handleSubmit(event) {
     event.preventDefault()
-    axios.post(`http://localhost:3001/new`, {
+    axios.post(`https://webfotograph-project.herokuapp.com/new`, {
      name: this.state.name,
      detail:this.state.detail,
      gallery: this.state.gallery
